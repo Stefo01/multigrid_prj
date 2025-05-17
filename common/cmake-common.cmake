ab4f908f20ee77b243fa5a4a8cb48af4c38e84b9
denisuntila <denis.untila@mail.polimi.it>
2025-05-17 23:45:46 +0200

    Modified CMakeLists

---
+++ common/cmake-common.cmake
@@ -0,0 +1,25 @@
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED "ON")
+
+# Set default build type to Release.
+if(NOT CMAKE_BUILD_TYPE OR "${CMAKE_BUILD_TYPE}" STREQUAL "")
+  set(CMAKE_BUILD_TYPE "Release" CACHE STRING "" FORCE)
+endif()
+
+message(STATUS)
+message(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
+message(STATUS)
+
+if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+  add_definitions(-DBUILD_TYPE_DEBUG)
+endif()
+
+# Locate MPI compiler
+find_package(MPI REQUIRED)
+set(CMAKE_CXX_COMPILER "${MPI_CXX_COMPILER}")
+
+# add here include directories
+
+
+
+
